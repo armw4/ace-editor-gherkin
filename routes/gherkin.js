50b8bd8d2a3bce6b4044c32b09d3f6454860c7ad
Antwan Wimberly <antwan.wimberly@gmail.com>
2017-06-26 11:09:43 -0400

    Post content to api on save

--- routes/gherkin.js
+++ routes/gherkin.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const cucumber = require('cucumber')
+const fetch = require('isomorphic-fetch')
 
 /* GET gherkin listing. */
-router.get('/', function(req, res, next) {
+router.get('/:issueKey', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/', (req, res) => {
-  res.sendStatus
+// create new
+router.put('/:issueKey', (req, res) => {
+  console.log(req.body)
+  res.send({ key: 'value' })
+})
+
+// update existing
+router.post('/:issueKey', (req, res) => {
+  console.log(req.body)
+  res.sendStatus(200)
 })
 
 module.exports = router;
