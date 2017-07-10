fca85a1c793ef4d4b3119feed5fbec2cc5628d35
Antwan Wimberly <antwan.wimberly@gmail.com>
2017-07-09 21:44:52 -0400

    Begin live error reporting for gherkin syntax (add worker)

---
+++ public/javascripts/worker-gherkin.js
@@ -0,0 +1,38 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  var oop = require('ace/lib/oop');
+  var Mirror = require('ace/worker/mirror').Mirror;
+  var lint = new Gherkin.Parser();
+
+  lint.stopAtFirstError = false;
+
+  var WorkerModule = exports.WorkerModule = function(sender) {
+    Mirror.call(this, sender);
+    this.setTimeout(500);
+    this.setOptions();
+  };
+
+  oop.inherits(WorkerModule, Mirror);
+
+  (function() {
+    this.onUpdate = function() {
+      var value = this.doc.getValue();
+
+      try {
+        lint(value);
+      } catch(e) {
+        var errors = e.errors.map(function(error) {
+          return {
+            row: error.location.line - 1, // must be 0 based
+            column: error.location.column - 1,
+            text: error.message,
+            type: 'error'
+          };
+        });
+
+        this.sender.emit('lint', errors);
+      }
+    };
+  }).call(WorkerModule.prototype);
+});
