066028839ee74423b0ce06800a906497af7ad8c6
Antwan Wimberly <antwan.wimberly@gmail.com>
2017-07-17 16:27:10 -0400

    Await step.exists as count() yields a promise

--- routes/gherkin.js
+++ routes/gherkin.js
@@ -11,7 +11,7 @@ const camelize = require('camelize');
 router.get('/:issueKey', async (req, res) => {
   try {
     const { issueKey } = req.params;
-    const stepsExist = step.exists(issueKey, req.organizationId);
+    const stepsExist = await step.exists(issueKey, req.organizationId);
 
     if (!stepsExist) {
       return res.sendStatus(404, 'Not Found');
@@ -30,7 +30,7 @@ router.put('/:issueKey', async (req, res) => {
     const { issueKey } = req.params;
     const { content } = req.body;
     const { organizationId } = req;
-    const stepsExist = step.exists(issueKey, organizationId);
+    const stepsExist = await step.exists(issueKey, organizationId);
     const githubWrite = stepsExist ? github.updateFeatureFile : github.createFeatureFile;
     const { feature } = gherkin.parse(content);
     const { background: { backgroundSteps }, scenarios } = feature;
