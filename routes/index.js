b8f9534a34c806ea702929fbaac687de29f15974
Antwan Wimberly <antwan.wimberly@gmail.com>
2017-07-19 12:07:50 -0400

    Don't render a title

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-const index = (req, res, next) => res.render('index', { title: 'Express' })
+const index = (req, res, next) => res.render('index')
 
 router.get('/', index);
 
